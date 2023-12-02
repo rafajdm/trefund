3adfaecce9c6fce8d239924587c4c999cf0b3883
Rafael Jose D <diazrafael18@gmail.com>
2023-12-02 13:00:10 -0300

    Initial commit

---
+++ src/main/java/aw/hackathon/avinnovate/trefund/TRefundApplication.java
@@ -0,0 +1,13 @@
+package aw.hackathon.avinnovate.trefund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TRefundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TRefundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aw/hackathon/avinnovate/trefund/TRefundApplicationTests.java
@@ -0,0 +1,13 @@
+package aw.hackathon.avinnovate.trefund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TRefundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
